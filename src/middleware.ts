812afa67ee56ab07cb7bb709c9a60e85fdf2ed99
Damir Bajrami <bajrami.damir@gmail.com>
2025-02-11 22:55:45 +0100

    using jose on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
 import { NextResponse, NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
+import { jwtVerify } from "jose";
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
   const { pathname } = new URL(req.url);
 
   if (pathname === "/login") {
+    console.log("User is on login page. Skipping middleware.");
     return NextResponse.next();
   }
 
@@ -15,8 +16,9 @@ export async function middleware(req: NextRequest) {
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-    console.log("Token is valid:", decoded);
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
+    const { payload } = await jwtVerify(token, secret);
+    console.log("Token is valid:", payload);
   } catch (error) {
     console.error("Invalid or expired token:", error);
     return NextResponse.redirect(new URL("/login", req.url));
